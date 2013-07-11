c3c18d20d5956afa01ce302b2aa352d768a4e561
Erica Cheong <erica.clw@gmail.com>
2013-07-12 03:08:23 +0800

    update web.js. read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var infile = "index.html";
+
+// read from file
+var text = fs.readFileSync(infile).toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
